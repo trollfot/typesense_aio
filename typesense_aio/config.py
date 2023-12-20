5056ad29b0cd999120cf2ddb70eded188558334d
Souheil Chelfouh <trollfot@gmail.com>
2023-12-21 00:39:58 +0100

    Timeout from 3 to 5 by default

--- typesense_aio/config.py
+++ typesense_aio/config.py
@@ -4,7 +4,7 @@ from typing import List, NamedTuple
 class Configuration(NamedTuple):
     urls: List[str]
     api_key: str
-    timeout: float = 3.0
+    timeout: float = 5.0
     retries: int = 3
     retry_interval: float = 1.0
     healthcheck_interval: float = 60
